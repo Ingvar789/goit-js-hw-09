2d6cc6cdf9c64811c792abd29cebc18fdc449f4c
Ihor Bodianchuk <ihordobruy@gmail.com>
2023-01-31 16:34:56 +0200

    added hw9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,29 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
-// const log = time => console.log('log every ${time}ms');
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
 
-// setInterval(log, 2000, 2000);
-console.log('s');
+btnStop.setAttribute('disabled', '');
+const COLOR_DELAY = 1000;
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartClick() {
+    btnStart.setAttribute('disabled', '');
+    btnStop.removeAttribute('disabled', '');
+    body.style.backgroundColor = getRandomHexColor();
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, COLOR_DELAY);
+}
+
+function onStopClick() {
+    btnStop.setAttribute('disabled', '');
+    btnStart.removeAttribute('disabled', '');
+    clearInterval(intervalId);
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+const refs = {
+    formInput:document.querySelector('.form'),
+    createtBtn: document.querySelector('button'),
+};
+
+const data = {};
+
+refs.formInput.addEventListener('input', onInput);
+refs.createtBtn.addEventListener('click', onCreateBtnSubmit);
+
+function onInput(e) {
+    data[e.target.name] = Number(e.target.value);
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+function onCreateBtnSubmit(e) {
+    e.preventDefault();
+    for (let position = 1; position <= data.amount; position += 1) { 
+            createPromise(position, data.delay)
+            .then(({position, delay}) => Notiflix.Notify.success((`✅ Fulfilled promise ${position} in ${delay}ms`)))
+            .catch(({ position, delay }) => Notiflix.Notify.failure((`❌ Rejected promise ${position} in ${delay}ms`)));
+        data.delay += data.step;   
+    }
+    }
